3bfb66eb933bcf5411900b976d6a791ea3746ee7
Ayush Singh <116262166+AyushSin-gh@users.noreply.github.com>
2026-01-30 20:56:40 +0530

    Update CricketerServiceImplJdbc.java

--- server/src/main/java/com/edutech/progressive/service/impl/CricketerServiceImplJdbc.java
+++ server/src/main/java/com/edutech/progressive/service/impl/CricketerServiceImplJdbc.java
@@ -1,50 +1,97 @@
-package com.edutech.progressive.service.impl;
+// package com.edutech.progressive.service.impl;
 
-import java.util.ArrayList;
-import java.util.List;
+// import java.util.ArrayList;
+// import java.util.List;
+
+// import com.edutech.progressive.dao.CricketerDAO;
+// import com.edutech.progressive.dao.CricketerDAOImpl;
+// import com.edutech.progressive.entity.Cricketer;
+// import com.edutech.progressive.service.CricketerService;
+
+// public class CricketerServiceImplJdbc implements CricketerService  {
+//     private CricketerDAO cricketerDAO=new CricketerDAOImpl();
+
+//     @Override
+//     public Integer addCricketer(Cricketer cricketer) {
+//         // TODO Auto-generated method stub
+//         return -1;
+//     }
+
+//     @Override
+//     public void deleteCricketer(int cricketerId) {
+//         // TODO Auto-generated method stub
+//        // CricketerService.super.deleteCricketer(cricketerId);
+//     }
+
+//     @Override
+//     public List<Cricketer> getAllCricketers() {
+//         // TODO Auto-generated method stub
+//         return new ArrayList<>();
+//     }
+
+//     @Override
+//     public List<Cricketer> getAllCricketersSortedByExperience() {
+//         // TODO Auto-generated method stub
+//         return new ArrayList<>();
+//     }
+
+//     @Override
+//     public Cricketer getCricketerById(int cricketerId) {
+//         // TODO Auto-generated method stub
+//         return null;
+//     }
+
+//     @Override
+//     public void updateCricketer(Cricketer cricketer) {
+//         // TODO Auto-generated method stub
+//         //CricketerService.super.updateCricketer(cricketer);
+//     }
+    
+// }
+
+package com.edutech.progressive.service;
 
 import com.edutech.progressive.dao.CricketerDAO;
 import com.edutech.progressive.dao.CricketerDAOImpl;
-import com.edutech.progressive.entity.Cricketer;
-import com.edutech.progressive.service.CricketerService;
+import com.edutech.progressive.model.Cricketer;
 
-public class CricketerServiceImplJdbc implements CricketerService  {
-    private CricketerDAO cricketerDAO=new CricketerDAOImpl();
+import java.sql.SQLException;
+import java.util.Comparator;
+import java.util.List;
+
+public class CricketerServiceImplJdbc implements CricketerService {
+
+    private CricketerDAO cricketerDAO = new CricketerDAOImpl();
 
     @Override
-    public Integer addCricketer(Cricketer cricketer) {
-        // TODO Auto-generated method stub
-        return -1;
+    public List<Cricketer> getAllCricketers() throws SQLException {
+        return cricketerDAO.getAllCricketers();
     }
 
     @Override
-    public void deleteCricketer(int cricketerId) {
-        // TODO Auto-generated method stub
-       // CricketerService.super.deleteCricketer(cricketerId);
+    public Integer addCricketer(Cricketer cricketer) throws SQLException {
+        return cricketerDAO.addCricketer(cricketer);
     }
 
     @Override
-    public List<Cricketer> getAllCricketers() {
-        // TODO Auto-generated method stub
-        return new ArrayList<>();
+    public List<Cricketer> getAllCricketersSortedByExperience() throws SQLException {
+        List<Cricketer> list = cricketerDAO.getAllCricketers();
+        list.sort(Comparator.comparingInt(Cricketer::getExperience).reversed());
+        return list;
     }
 
     @Override
-    public List<Cricketer> getAllCricketersSortedByExperience() {
-        // TODO Auto-generated method stub
-        return new ArrayList<>();
+    public void updateCricketer(Cricketer cricketer) throws SQLException {
+        cricketerDAO.updateCricketer(cricketer);
     }
 
     @Override
-    public Cricketer getCricketerById(int cricketerId) {
-        // TODO Auto-generated method stub
-        return null;
+    public void deleteCricketer(int cricketerId) throws SQLException {
+        cricketerDAO.deleteCricketer(cricketerId);
     }
 
     @Override
-    public void updateCricketer(Cricketer cricketer) {
-        // TODO Auto-generated method stub
-        //CricketerService.super.updateCricketer(cricketer);
+    public Cricketer getCricketerById(int cricketerId) throws SQLException {
+        return cricketerDAO.getCricketerById(cricketerId);
     }
-    
 }
