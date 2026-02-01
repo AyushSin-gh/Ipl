7066cb1c5cf1a61b837f0e866b0ad3e021b63d09
Ayush Singh <116262166+AyushSin-gh@users.noreply.github.com>
2026-02-01 21:27:59 +0530

    Update CricketerServiceImplJdbc.java

--- server/src/main/java/com/edutech/progressive/service/impl/CricketerServiceImplJdbc.java
+++ server/src/main/java/com/edutech/progressive/service/impl/CricketerServiceImplJdbc.java
@@ -1,53 +1,4 @@
-// package com.edutech.progressive.service.impl;
 
-// import java.util.ArrayList;
-// import java.util.List;
-
-// import com.edutech.progressive.dao.CricketerDAO;
-// import com.edutech.progressive.dao.CricketerDAOImpl;
-// import com.edutech.progressive.entity.Cricketer;
-// import com.edutech.progressive.service.CricketerService;
-
-// public class CricketerServiceImplJdbc implements CricketerService  {
-//     private CricketerDAO cricketerDAO=new CricketerDAOImpl();
-
-//     @Override
-//     public Integer addCricketer(Cricketer cricketer) {
-//         // TODO Auto-generated method stub
-//         return -1;
-//     }
-
-//     @Override
-//     public void deleteCricketer(int cricketerId) {
-//         // TODO Auto-generated method stub
-//        // CricketerService.super.deleteCricketer(cricketerId);
-//     }
-
-//     @Override
-//     public List<Cricketer> getAllCricketers() {
-//         // TODO Auto-generated method stub
-//         return new ArrayList<>();
-//     }
-
-//     @Override
-//     public List<Cricketer> getAllCricketersSortedByExperience() {
-//         // TODO Auto-generated method stub
-//         return new ArrayList<>();
-//     }
-
-//     @Override
-//     public Cricketer getCricketerById(int cricketerId) {
-//         // TODO Auto-generated method stub
-//         return null;
-//     }
-
-//     @Override
-//     public void updateCricketer(Cricketer cricketer) {
-//         // TODO Auto-generated method stub
-//         //CricketerService.super.updateCricketer(cricketer);
-//     }
-    
-// }
 
 package com.edutech.progressive.service;
 
@@ -58,40 +9,37 @@ import com.edutech.progressive.model.Cricketer;
 import java.sql.SQLException;
 import java.util.Comparator;
 import java.util.List;
+import java.sql.SQLException;
+import java.util.*;
 
-public class CricketerServiceImplJdbc implements CricketerService {
+public class CricketerServiceImplJdbc {
 
     private CricketerDAO cricketerDAO = new CricketerDAOImpl();
 
-    @Override
     public List<Cricketer> getAllCricketers() throws SQLException {
         return cricketerDAO.getAllCricketers();
     }
 
-    @Override
     public Integer addCricketer(Cricketer cricketer) throws SQLException {
         return cricketerDAO.addCricketer(cricketer);
     }
 
-    @Override
     public List<Cricketer> getAllCricketersSortedByExperience() throws SQLException {
         List<Cricketer> list = cricketerDAO.getAllCricketers();
-        list.sort(Comparator.comparingInt(Cricketer::getExperience).reversed());
+        list.sort(Comparator.comparingInt(Cricketer::getExperience));
         return list;
     }
 
-    @Override
     public void updateCricketer(Cricketer cricketer) throws SQLException {
         cricketerDAO.updateCricketer(cricketer);
     }
 
-    @Override
-    public void deleteCricketer(int cricketerId) throws SQLException {
-        cricketerDAO.deleteCricketer(cricketerId);
+    public void deleteCricketer(int id) throws SQLException {
+        cricketerDAO.deleteCricketer(id);
     }
 
-    @Override
-    public Cricketer getCricketerById(int cricketerId) throws SQLException {
-        return cricketerDAO.getCricketerById(cricketerId);
+    public Cricketer getCricketerById(int id) throws SQLException {
+        return cricketerDAO.getCricketerById(id);
     }
 }
+
