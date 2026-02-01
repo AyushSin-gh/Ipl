2f893fbc6e36c70d33f191c05636d55b8551370c
Ayush Singh <116262166+AyushSin-gh@users.noreply.github.com>
2026-02-01 21:36:10 +0530

    Update MatchServiceImplJdbc.java

--- server/src/main/java/com/edutech/progressive/service/impl/MatchServiceImplJdbc.java
+++ server/src/main/java/com/edutech/progressive/service/impl/MatchServiceImplJdbc.java
@@ -9,27 +9,39 @@ import com.edutech.progressive.service.MatchService;
 import java.sql.SQLException;
 import java.util.List;
 
-public class MatchServiceImplJdbc {
+import java.sql.SQLException;
+import java.util.List;
+
+public class MatchServiceImplJdbc implements MatchService {
 
     private MatchDAO matchDAO = new MatchDAOImpl();
 
+    @Override
     public List<Match> getAllMatches() throws SQLException {
         return matchDAO.getAllMatches();
     }
 
-    public Match getMatchById(int id) throws SQLException {
-        return matchDAO.getMatchById(id);
+    @Override
+    public Match getMatchById(int matchId) throws SQLException {
+        return matchDAO.getMatchById(matchId);
     }
 
+    @Override
     public Integer addMatch(Match match) throws SQLException {
         return matchDAO.addMatch(match);
     }
 
+    @Override
     public void updateMatch(Match match) throws SQLException {
         matchDAO.updateMatch(match);
     }
 
-    public void deleteMatch(int id) throws SQLException {
+    @Override
+    public void deleteMatch(int matchId) throws SQLException {
+        matchDAO.deleteMatch(matchId);
+    }
+}
+deleteMatch(int id) throws SQLException {
         matchDAO.deleteMatch(id);
     }
 }
